757586f79516d2f74fb6d22139ece47a0cbfe09a
Ali George <george7ali@gmail.com>
2025-01-28 01:05:25 +0100

    import the services section component to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ServicesSection from "./components/ServicesSection";
 
 function App() {
   return (
     <div className="overflow-hidden">
       <Navbar />
       <HeroSection />
+      <ServicesSection />
     </div>
   );
 }
