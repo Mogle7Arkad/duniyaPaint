0cf1935c66bce83635b9cd88fef3b5af4f0a0cb6
Ali George <george7ali@gmail.com>
2025-01-30 16:50:39 +0100

    edit bg color of button and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <a href="#" className="flex items-center gap-1 text-2xl font-bold text-blue-400">
+        <a href="#" className="flex items-center gap-1 text-2xl font-bold text-purple-400">
             <img className="h-12 w-12" src="../assets/painting-home-svgrepo-com.svg" alt="" />
           Duniya Finishes
         </a>
