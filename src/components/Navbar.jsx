b3e400d65be764ff5b3b783c555ba206629c942a
Ali George <george7ali@gmail.com>
2025-02-09 12:33:30 +0100

    add logo to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <a href="#" className="flex items-center gap-1 text-2xl font-bold text-purple-400">
-            <img className="h-12 w-12" src="../assets/painting-home-svgrepo-com.svg" alt="" />
+        <a href="#" className="flex items-center gap-1 text-2xl font-bold text-gray-800">
+            <img className="h-14 w-14" src="../assets/logo2.png" alt="" />
           Duniya Finishes
         </a>
         <DesktopNav />
